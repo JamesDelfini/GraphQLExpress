73b48929b74c483e282158c665d33f72212995f6
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-18 01:57:21 +0800

    GraphQL Setup Get Data of Books and Authors Schema

--- server.js
+++ server.js
@@ -1,25 +1,74 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 
+const {authors, books} = require('./dummydata');
+
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
 } = require('graphql');
 
 const app = express();
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: { 
-                type: GraphQLString ,
-                resolve: () => 'Hello World'
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+        authors: {
+            type: AuthorType,
+            resolve: (book) => {
+                return authors.find(author => author.id === book.authorId);
             }
-        })
+        }
     })
 });
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents an author of a book',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        books : {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter(book => book.authorId === author.id);
+            }
+        }
+    })
+});
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: { 
+            type: new GraphQLList(BookType),
+            description: 'List of All Books',
+            resolve: () => books // can be used here to query to a database
+            // resolve: function(){
+            //     throw ('TEST ERROR');
+            //     return books
+            // }
+        },
+        authors: { 
+            type: new GraphQLList(AuthorType),
+            description: 'List of All Authors',
+            resolve: () => authors
+        }
+    })
+});
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
 
 app.use('/graphql', expressGraphQL({
     schema: schema,
